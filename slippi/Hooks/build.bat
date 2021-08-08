51a2f0ea2996afc171423238ca29a31e8ab746d2
Robert Peralta <rapito@gmail.com>
2021-08-08 02:07:38 -0400

    Remove pause when building

--- slippi/Hooks/build.bat
+++ slippi/Hooks/build.bat
@@ -2,5 +2,3 @@ SET "TKPATH=../../MexTK/"
 SET "FSPATH=C:\Users\rober\Documents\Dev\Github\Ishiiruka\Binary\x64\Sys\GameFiles\GALE01\"
 :: compile main code
 "%TKPATH%MexTK.exe" -ff -i "main.c" -s slpFunction -t "%TKPATH%slpFunction.txt" -l "%TKPATH%melee.link" -o "%FSPATH%Slippi.dat" -v -ow
-
-pause
